402dd2636f9a809301f29a63b5c5727941b46fb9
GryNiS <GryNiSNe@gmail.com>
2016-02-27 01:57:15 +0200

    ButterflyClass

---
+++ Homework/Lesson1/Part2/Butterfly.java
@@ -0,0 +1,24 @@
+public class Animals {
+	private int age;
+	private int weight;
+	public String type;
+	public String color;
+	public int number of legs;
+	public int number of wings;
+	public int number of antennae;
+	public int number of eyes;	
+	public String name;
+	public String nature;
+	public boolean cyclone=false;
+	
+	void private flit() {}
+	void public pollinate() {}
+	void private eat () {}
+	void private drink() {}
+	void private sniff() {}
+	void private fly () {}
+	public boolean create cyclone(boolean cyclone) {cyclone=true; return cyclone;}
+	void private crawl() {}
+	void private sleep () {}
+	private int grow(int age) {age=age+1; return age;}
+}
