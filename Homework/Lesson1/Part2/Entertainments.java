e58eff27a9c552ec40e25622a2c1d02c71ac9a32
GryNiS <GryNiSNe@gmail.com>
2016-02-29 10:30:06 +0200

    Entertainments

---
+++ Homework/Lesson1/Part2/Entertainments.java
@@ -0,0 +1,23 @@
+public class Entertainments {
+	private int minimum number of people;
+	private int maximum number of people;
+	public String type;
+	public String difficulty level;
+	public int cost;
+	public int time;
+	public boolean for_men=false;
+	public int age restrictions;	
+	public boolean for_women=false;
+	public boolean for_children=false;
+	
+	void private peopleRunning() {}
+	void public laughing() {}
+	void private joking () {}
+	void private havingFun() {}
+	void private swim() {}
+	void private jump () {}
+	void public ride(}
+	void private eat() {}
+	void private read () {}
+	void private watch(){}
+}
