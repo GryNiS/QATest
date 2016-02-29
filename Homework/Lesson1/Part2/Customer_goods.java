65e76478223a3a07893089a3268945a3cadafaae
GryNiS <GryNiSNe@gmail.com>
2016-02-29 10:23:24 +0200

    Goods is good

---
+++ Homework/Lesson1/Part2/Customer_goods.java
@@ -0,0 +1,23 @@
+public class Animals {
+	private int barcode;
+	private int weight;
+	public String type;
+	public String color;
+	public int cost;
+	public int date_of_manufacture;
+	public int guarantee_period;
+	public int size;	
+	public String name;
+	public boolean eatable=false;
+	
+	void private buy() {}
+	void public weighing() {}
+	void private sell () {}
+	void private addToTheDatabase() {}
+	void private deliver() {}
+	void private support () {}
+	void public warrantyExtension(}
+	void private getInformation() {}
+	void private eat () {}
+	void private steal(){}
+}
