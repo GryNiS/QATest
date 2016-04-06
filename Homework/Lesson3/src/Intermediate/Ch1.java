db067dcecf7c41a5bb31e94854823d045d0013b2
GryNiS <GryNiSNe@gmail.com>
2016-04-07 00:05:57 +0300

    Update Ch1.java
    rebuild

--- Homework/Lesson3/src/Intermediate/Ch1.java
+++ Homework/Lesson3/src/Intermediate/Ch1.java
@@ -5,22 +5,14 @@ import java.util.Scanner;
 public class Ch1 {
 
 	public static void main(String[] args) {
-		 int n = 12345;
-		 int sum = 0;
-		 System.out.println("Чему равна сумма всех цифр числа "+n+" ?");
-		 Scanner sc = new Scanner(System.in);
-		 for(n = n; n != 0; n /= 10)
+		int sum = 0;
+		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(n = n; n != 0; n /= 10)
 			{ 
 			 	sum = sum + (n % 10); 
 			} 
-		 int sn = sc.nextInt();
-		 if (sum==sn) {
-			 System.out.println("Угадал");
-		 }
-		 else{
-			 System.out.println("Прогадал");
-		 }
-		 	
-	}
+		System.out.println(sum);
 
 }
