543e33fd35690610eeea30d8c0d8351787f5dad8
GryNiS <GryNiSNe@gmail.com>
2016-04-07 00:00:20 +0300

    Update Ch7.java
    rebuild

--- Homework/Lesson3/src/Begin/Ch7.java
+++ Homework/Lesson3/src/Begin/Ch7.java
@@ -7,7 +7,7 @@ public class Ch7 {
 		int arr[] = {100,2,6,200,-1,-100,10};
 		int maximum=arr[0];
 		for(int i = 1; i<arr.length;i++){
-			maximum=Math.max(maximum, arr[i]);
+			maximum=(maximum>arr[i])?maximum:arr[i];
 		}
 	}
 
