1b29734c5692016a1428bf288ee7552ca90dd8f5
GryNiS <GryNiSNe@gmail.com>
2016-04-07 00:16:04 +0300

    Update Chos.java
    rebuild

--- Homework/Lesson3/src/Intermediate/Chos.java
+++ Homework/Lesson3/src/Intermediate/Chos.java
@@ -4,11 +4,17 @@ public class Chos {
 int z;
 	double toGradus(double a){
 		a-=273.15;
+		if (a<-545,3){
+		System.out.println("Less absolute zero");
+		}
 		return a;
 	}
 	
 	double toKelvin(double a){
 		a+=273.15;
+		if (a<-273.15){
+		System.out.println("Less absolute zero");
+		}
 		return a;
 	}
 
