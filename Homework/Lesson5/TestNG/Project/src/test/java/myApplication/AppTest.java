30bc122a43ce1d86ac9abc8d264a890e9499c855
GryNiS <GryNiSNe@gmail.com>
2016-05-05 10:42:57 +0300

    l5

--- Homework/Lesson5/TestNG/Project/src/test/java/myApplication/AppTest.java
+++ Homework/Lesson5/TestNG/Project/src/test/java/myApplication/AppTest.java
@@ -8,57 +8,57 @@ public class AppTest{
 
 	private App app = new App();
 	
-    @Test
+    @Test(timeOut = 10000, groups = "testTimer")
     public void book() throws Exception {
     	app.ReadBook();
     }
     
-    @BeforeMethod
+    @BeforeMethod(enabled = true, groups = "checkTestNG")
     public void setUp() throws Exception {
         System.out.println("@BeforeMethod");
     }
  
-    @AfterMethod
+    @AfterMethod(enabled = true, groups = "checkTestNG")
     public void tearDown() throws Exception {
         System.out.println("@AfterMethod");
     }
  
-    @BeforeGroups
+    @BeforeGroups(enabled = false, groups = "checkTestNG")
     public void beforeGroups() throws Exception {
         System.out.println("BeforeGroups");
     }
  
-    @AfterGroups
+    @AfterGroups(enabled = false, groups = "checkTestNG")
     public void afterGroups() throws Exception {
         System.out.println("@AfterGroups");
     }
  
-    @BeforeClass
+    @BeforeClass(enabled = true, groups = "checkTestNG")
     public void beforeClass() throws Exception {
         System.out.println("@BeforeClass");
     }
  
-    @BeforeTest
+    @BeforeTest(enabled = false, groups = "checkTestNG")
     public void beforeTest() throws Exception {
         System.out.println("@BeforeTest");
     }
  
-    @AfterTest
+    @AfterTest(enabled = true, groups = "checkTestNG")
     public void afterTest() throws Exception {
         System.out.println("@AfterTest");
     }
  
-    @AfterClass
+    @AfterClass(enabled = false, groups = "checkTestNG")
     public void afterClass() throws Exception {
         System.out.println("@AfterClass");
     }
  
-    @BeforeSuite
+    @BeforeSuite(enabled = true, groups = "checkTestNG")
     public void beforeSuite() throws Exception {
         System.out.println("@BeforeSuite");
     }
  
-    @AfterSuite
+    @AfterSuite(enabled = false, groups = "checkTestNG")
     public void afterSuite() throws Exception {
         System.out.println("@AfterSuite");
     }
